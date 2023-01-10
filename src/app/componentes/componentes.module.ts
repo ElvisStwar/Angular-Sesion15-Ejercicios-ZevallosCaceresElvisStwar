0e9668788320ee011b640d277e29a676fabe268e
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-10 09:51:27 -0500

    Creacion de modulos y componenetes

---
+++ src/app/componentes/componentes.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from './hijo/hijo.component';
+
+
+
+@NgModule({
+  declarations: [
+    PadreComponent,
+    HijoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentesModule { }
