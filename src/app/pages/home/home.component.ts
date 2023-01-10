0d6bfdd611cec8528d5a6aa87237e96803a6f081
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-10 10:54:09 -0500

    Utilizando dataBinding para el titulo y mostrar/ocultar parrafo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  titulo:string="Data Binding Interpolation";
+  parrafo:string="";
+  num=0;
+  boton="Mostrar"
+
+  mostrar(){
+    this.num= this.num+1
+    if(this.num%2!=0){
+      this.parrafo="Event Binding es el mecanismo de data binding mediante el cual trabajan con los eventos que se encuentran en el DOM.";
+      this.boton="Ocultar";
+    }else{
+      this.parrafo=""
+      this.boton="Mostrar";
+    }
+  }
 }
